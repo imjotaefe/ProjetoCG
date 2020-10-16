ad3aa7767c85e90f11380573154dc171b2d28f63
João Fernando Garcia de Souza <jotaefegds@gmail.com>
2020-10-16 19:59:27 -0300

    feat: initial commit

---
+++ Assets/camera_controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour
+{
+    public float unidadeRx, unidadeRy;
+    float vr;
+    public GameObject player;
+
+    void Start()
+    {
+        vr = 50f;
+        unidadeRx = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Update()
+    {
+        unidadeRx += Input.GetAxis("Mouse Y") * vr * Time.deltaTime;
+        unidadeRx = Mathf.Clamp(unidadeRx, -60f, 60f);
+        transform.localEulerAngles = new Vector3(-unidadeRx, 0, 0);
+
+        unidadeRy = Input.GetAxis("Mouse X") * vr * Time.deltaTime;
+        player.transform.Rotate(0, unidadeRy, 0);
+    }
+}
---
+++ Assets/player_controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    public float velocidade = 5;
+
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "gem" )
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "box")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+
+        transform.Translate(moveHorizontal, 0, moveVertical);
+    }
+}
